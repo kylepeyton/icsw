3a269a2a858cf93427e74bd777496add8e67783f
Kyle Peyton <kyle.peyton@yale.edu>
2015-05-15 15:59:42 -0400

    fuck this is ridiculous

---
+++ Desktop/scratch.R
@@ -0,0 +1,41 @@
+install.packages("devtools")
+install.packages("Rcpp")
+
+library("Rcpp")
+library("devtools")
+
+devtools::install_github("klutometis/roxygen")
+library(roxygen2)
+
+
+setwd("~/Kyle/Dropbox/icsw")
+create("icsw")
+
+
+install_github('kylepeyton/icsw')
+
+devtools::install_github("kylepeyton/icsw")
+
+
+install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
+
+require(devtools);require(RCurl)
+install.packages('RCurl')
+devtools::install_github("kylepeyton/icsw", auth_token = "cdf3efed157fd75ebf286e32f47457a427885b2a")
+install_github("kylepeyton/icsw", auth_token = "cdf3efed157fd75ebf286e32f47457a427885b2a")
+install_github("kylepeyton/shiny_surveys")
+
+install_git("https://github.com/kylepeyton/icsw.git", auth_token = "cdf3efed157fd75ebf286e32f47457a427885b2a")
+
+
+install_github("deaneckles/icsw")
+
+
+install_git("https://github.com/deaneckles/icsw.git")
+install_github("kbroman/broman")
+
+
+
+
+
+
